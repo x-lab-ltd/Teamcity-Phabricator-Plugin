13e6efd89e407752c11269805d3146994cac7ecd
Steven Cooney <steven.cooney@x-labsystems.co.uk>
2019-05-31 14:45:52 +0100

    Change logging format.
    Does what it says on the tin

--- Teamcity-Phabricator-Plugin/phabricator-plugin-common/src/main/java/uk/xlab/teamcity/phabricator/PhabricatorAgentLogger.java
+++ Teamcity-Phabricator-Plugin/phabricator-plugin-common/src/main/java/uk/xlab/teamcity/phabricator/PhabricatorAgentLogger.java
@@ -6,16 +6,16 @@ public final class PhabricatorAgentLogger implements IPhabricatorPluginLogger {
 
     @Override
     public void info(String message) {
-        Loggers.AGENT.info(String.format("Phabricator Plugin: %s", message));
+        Loggers.AGENT.info(String.format("Phabricator Plugin - %s", message));
     }
 
     @Override
     public void warn(String message, Exception e) {
-        Loggers.AGENT.warn(String.format("Phabricator Plugin: %s", message), e);
+        Loggers.AGENT.warn(String.format("Phabricator Plugin - %s", message), e);
     }
 
     @Override
     public void error(String message, Exception e) {
-        Loggers.AGENT.error(String.format("Phabricator Plugin: %s", message), e);
+        Loggers.AGENT.error(String.format("Phabricator Plugin - %s", message), e);
     }
 }
--- Teamcity-Phabricator-Plugin/phabricator-plugin-common/src/main/java/uk/xlab/teamcity/phabricator/PhabricatorServerLogger.java
+++ Teamcity-Phabricator-Plugin/phabricator-plugin-common/src/main/java/uk/xlab/teamcity/phabricator/PhabricatorServerLogger.java
@@ -6,16 +6,16 @@ public final class PhabricatorServerLogger implements IPhabricatorPluginLogger {
 
     @Override
     public void info(String message) {
-        Loggers.SERVER.info(String.format("Phabricator Plugin: %s", message));
+        Loggers.SERVER.info(String.format("Phabricator Plugin - %s", message));
     }
 
     @Override
     public void warn(String message, Exception e) {
-        Loggers.SERVER.warn(String.format("Phabricator Plugin: %s", message), e);
+        Loggers.SERVER.warn(String.format("Phabricator Plugin - %s", message), e);
     }
 
     @Override
     public void error(String message, Exception e) {
-        Loggers.SERVER.error(String.format("Phabricator Plugin: %s", message), e);
+        Loggers.SERVER.error(String.format("Phabricator Plugin - %s", message), e);
     }
 }
